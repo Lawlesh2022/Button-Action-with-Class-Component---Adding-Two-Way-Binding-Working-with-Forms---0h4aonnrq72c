cef8c7e6a32524e6fae926dc33160b6f3ce49f58
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-09-01 23:44:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,26 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {	
-    constructor(props) {
-	super(props);
-	this.state = {
+      constructor(props) {
+	  super(props);
+	  this.state = {
 	   text:""
-	}
+	 }
    
-   };
-   handleButton = ()=>{
-       this.setState({
+     };
+     handleButton = ()=>{
+        this.setState({
           text : "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"   
-       })
-  }
-  render() {
-      return(
-    	<div id="main">
+        })
+    }
+    render() {
+       return(
+    	   <div id="main">
 	   <p id = "para">{ this.state.text }</p>
-	   <button id = "click" onClick = {this.handleButton}>Submit</button>
-    	</div>
-    );
-  }
+	   <button id = "click" onClick = {()=>this.handleButton()}>Submit</button>
+    	   </div>
+      );
+   }
 }
 
 
